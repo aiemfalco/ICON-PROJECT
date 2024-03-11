ef79826e83a831867da3560d604f1418ce2725cd
c.cristallo <c.cristallo6@studenti.uniba.it>
2024-03-11 17:14:37 +0100

    tolte righe inutili lol

--- main.py
+++ main.py
@@ -10,15 +10,11 @@ features_to_delete.append(features[3]) # stringa comp prima inserita
 for i in range(31, len(features)):
     features_to_delete.append(features[i]) #stringhe che non ci interessano (features)
 
-index_to_delete = [] # indici nel dataset delle features che vogliamo rimuovere
-for i in range(0, len(features_to_delete)):
-    index_to_delete.append(dataset.columns.get_loc(features_to_delete[i]))
 
 del features[3]
 del features[31:50]
 
 print("Features che vogliamo mantenere: ", features)
-print("Indici da rimuovere: ", index_to_delete)
 print("Features da rimuovere: ", features_to_delete)
 print("\n\n")
 
