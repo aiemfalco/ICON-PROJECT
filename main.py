0a72ff00146e58df73a736dbf35b0f4c4e6bfb98
RaffaeleArbues <raffaelearbues02@gmail.com>
2024-03-16 12:11:25 +0100

    aggiornamento

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ features = dataset.columns.tolist()
 
 features_to_delete = []
 features_to_delete.append(features[3]) # stringa comp prima inserita
+features_to_delete.append(features[14]) 
 for i in range(31, len(features) - 1):
     features_to_delete.append(features[i]) #stringhe che non ci interessano (features)
 
 
 del features[3]
+del features[14]
 del features[31:48]
+
 
 print("Features che vogliamo mantenere: ", features)
 print("Features da rimuovere: ", features_to_delete)
@@ -24,6 +27,10 @@ print("\n\n")
 
 # nel drop possiamo dargli sia il vettore di indici che il vettore di stringhe, si mangia tutto
 dataset = dataset.drop(features_to_delete, axis = 1)
+
+#andiamo ad eliminare tutti i valori null (ne rimanevano solamente 3 nella colonna "dist")
+dataset = dataset[dataset.isnull().sum(axis=1) == 0]
+  
 
 #11.03
 #preprocessing delle features stringhe in intero 
@@ -77,24 +84,34 @@ for tupla in dic_referees:
 referee_name = label_encoder.inverse_transform([referees_encoded[0]])
 print(referee_name)
 
-''' 
-for element in dataset['referee']:
-    for tupla in dic_referees:
-        for numero in tupla:
-            if dataset.loc[dataset['referee'] == element] == dic_referees.keys(element):
-                dataset.loc[dataset['referee'] == element] = dic_referees.get(numero)
-            
-
-                
 '''
+
+# qua andiamo a sostituire gli elementi che sono rappresentati come stringhe dentro il dataset con le chiavi dei rispettivi dizionari tramite la funzione di libreria "map"
+
+dataset["referee"] = dataset["referee"].map(dic_referees)
+dataset["date"] = dataset["date"].map(dic_dates)
+dataset["round"] = dataset["round"].map(dic_rounds)
+dataset["day"] = dataset["day"].map(dic_days)
+dataset["venue"] = dataset["venue"].map(dic_venues)
+dataset["result"] = dataset["result"].map(dic_results)
+dataset["opponent"] = dataset["opponent"].map(dic_opponents)
+dataset["captain"] = dataset["captain"].map(dic_captains)
+dataset["formation"] = dataset["formation"].map(dic_formations)
+dataset["team"] = dataset["team"].map(dic_teams)
+
+
+# droppiamo result perché nella x andiamo a mettere tutti i dati senza risultati, mentre nella y andiamo a mettere solo i risultati (x e y sono cloni del dataset)
 X = dataset.drop(columns=['result'])
 y = dataset['result']
+
 # problema di classificazione, creiamo un oggetto RandomForestClassifier
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 
+# qui andiamo a splittare i dataset in 4 (2) parti ossia l'x training e test e l'y training e test
 X_train, X_test, y_train, y_test = train_test_split(X, y, 0,2)
 print(X_test)
 
+'''
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 print(predictions)
