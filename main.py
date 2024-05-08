8887c9409c7ce5617d5cb7110b6eb6c7746eb9a6
RaffaeleArbues <raffaelearbues02@gmail.com>
2024-05-08 17:54:50 +0200

    avanzamento

--- main.py
+++ main.py
@@ -24,43 +24,20 @@ def is_a_valid_round(numero):
 def valid_time(ora, minuti):
     return bool(list(prolog.query(f"valid_time({ora}, {minuti})")))
 
+def cerca_stringa_in_dizionario(dizionario, stringa):
+    for chiave, valore in dizionario.items():
+        if stringa in chiave:
+            return valore
+    return None  # Se la stringa non è stata trovata in nessun valore del dizionario
+
 # metodo che prende in input la stringa di input che servirà come input al modello
 def get_input():
     user_input = []
     i = 0
-    for i in range(8):
+    for i in range(7):
         datoutente = ""
         exit = True
         if i==0:
-            while exit:
-                datoutente = input("Inserisci una data nel formato aaaa-mm-gg: ")
-                if len(datoutente) == 10 and datoutente[:4].isdigit() and datoutente[5:7].isdigit() and datoutente[8:].isdigit() and datoutente[4] == '-' and datoutente[7] == '-':
-                    yearG = int(datoutente[:4])
-                    monthG = int(datoutente[5:7])
-                    dayG = int(datoutente[8:])
-                    print(yearG, monthG, dayG)
-                    yearC = current_date.year
-                    monthC = current_date.month
-                    dayC = current_date.day
-                    print(yearC, monthC, dayC)
-                    # test partita futura in python
-                    if yearG > yearC:
-                        print("Input corretto")
-                        exit = False
-                    else:
-                        if yearG == yearC and monthG > monthC:
-                            print("Input corretto")
-                            exit = False
-                        else:
-                            if yearG == yearC and monthG == monthC and dayG > dayC:
-                                print("Input corretto")
-                                exit = False
-                            else :
-                                print("[!] Non hai inserito una data futura")
-                else:
-                    print("[!] La data deve essere in formato aaaa-mm-gg")
-
-        if i==1:
             while exit:
                 datoutente = input("Inserisci l'orario in cui si gioca la partita (nel formato hh:mm): ")
                 if len(datoutente) == 5 and datoutente[:2].isdigit() and datoutente[3:].isdigit() and datoutente[2] == ':':
@@ -76,7 +53,7 @@ def get_input():
                 else:
                     print("[!] L'ora deve essere in formato hh:mm")
 
-        if i==2:
+        if i==1:
             while exit:
                 datoutente = input("Inserisci la giornata: ")
                 if len(datoutente) > 0 and len(datoutente) < 3 and is_a_valid_round(datoutente):
@@ -85,7 +62,7 @@ def get_input():
                 else:
                     print("La giornata deve essere un numero compreso in [1, 38]")
 
-        if i==3:
+        if i==2:
             while exit:
                 datoutente = input("Inserisci il nome dello stadio in cui si giocherà la partita: ")
                 if len(datoutente) > 1:
@@ -94,7 +71,7 @@ def get_input():
                 else:
                     print("[!] È necessario inserire un nome")
 
-        if i==4:
+        if i==3:
             while exit:
                 datoutente=input("Inserisci la squadra che gioca in casa: ")
                 if len(datoutente) > 1:
@@ -103,7 +80,7 @@ def get_input():
                 else:
                     print("[!] È necessario inserire un nome")
 
-        if i==5:
+        if i==4:
             while exit:
                 datoutente=input("Inserisci la formazione (nel formato n-n-n o n-n-n-n): ")
                 if (len(datoutente) == 5 and datoutente[0].isdigit() and datoutente[2].isdigit() and datoutente[4].isdigit() and datoutente[1]=='-' and datoutente[3]=='-' and is_a_valid_number(datoutente[0]) and is_a_valid_number(datoutente[2]) and is_a_valid_number(datoutente[4])) or (len(datoutente) == 7 and datoutente[0].isdigit() and datoutente[2].isdigit() and datoutente[4].isdigit() and datoutente[6].isdigit() and datoutente[1]=='-' and datoutente[3]=='-' and datoutente[5]=='-' and is_a_valid_number(datoutente[0]) and is_a_valid_number(datoutente[2]) and is_a_valid_number(datoutente[4]) and is_a_valid_number(datoutente[6])):
@@ -112,7 +89,7 @@ def get_input():
                 else:
                     print("[!] Valori non accettati")
         
-        if i==6:
+        if i==5:
             while exit:
                 datoutente=input("Inserisci l'arbitro: ")
                 if len(datoutente) > 1:
@@ -121,7 +98,7 @@ def get_input():
                 else:
                     print("[!] È necessario inserire un nome")
         
-        if i==7:
+        if i==6:
             while exit:
                 datoutente=input("Inserisci la squadra che gioca in trasferta: ")
                 if len(datoutente) > 1:
@@ -132,14 +109,6 @@ def get_input():
         user_input.append(datoutente)
     return user_input
 
-'''def print_current_date():
-    current_date = list(prolog.query("print_current_date"))
-    if current_date:
-        print("Data corrente: ", current_date[0]['Date'])
-    else:
-        print("Operazione fallita")
-    return current_date '''
-
 # problema di classificazione, creiamo un oggetto RandomForestClassifier
 model = RandomForestClassifier(n_estimators = 50, min_samples_split = 10, random_state = 1)
 
@@ -147,7 +116,7 @@ model = RandomForestClassifier(n_estimators = 50, min_samples_split = 10, random
 dataset = ds.create_data_frame() # clono il dataset senza la colonna result
 X = dataset.loc[dataset[1] <= 430]
 X_train = X.drop(6, axis = 1) # prendo tutti i games prima della data 430 (escluso result chiaramente)
-X_train = X.loc[:, [29, 9, 1, 3, 15, 5, 2, 14]]
+X_train = X.loc[:, [29, 9, 3, 15, 5, 2, 14]]
 y_train = X.iloc[:, 6]
 
 # [!] spostare la colonna 5(result) come ultima colonna per comodità
@@ -155,7 +124,7 @@ X = dataset.loc[dataset[1] > 430] # prendo tutti i games successivi alla data 43
 X_test = X.drop(6, axis = 1)
 colonne_da_modificare = [0, 4, 7, 8, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
 X_test[colonne_da_modificare] = X_test[colonne_da_modificare].fillna(0)
-X_test = X.loc[:, [29, 9, 1, 3, 15, 5, 2, 14]]
+X_test = X.loc[:, [29, 9, 3, 15, 5, 2, 14]]
 y_test = X.iloc[:, 6]
 
 model.fit(X_train, y_train) # alleno il modello dandogli X e i result di X per ottenere un modello in grado di darci risposte
@@ -170,20 +139,31 @@ current_date = datetime.now()
 
 game = get_input()
 print("pre game " , game)
-# game(data, ora, giornata, stadio, casa, formazione, arbitro, trasferta)
-# gameT(casa, trasferta, data, giornata, arbitro, stadio, ora, formazione)
+# game(ora, giornata, stadio, casa, formazione, arbitro, trasferta)
+# gameT(casa, trasferta, giornata, arbitro, stadio, ora, formazione)
 temp = []
-temp.append(game[4])
-temp.append(game[7])
-temp.append(game[0])
-temp.append(game[2])
-temp.append(game[6])
-temp.append(game[3])
-temp.append(game[1])
-temp.append(game[5])
+temp.append(game[3]) #dic_teams
+temp.append(game[6]) #dic_opponents
+temp.append(game[1]) #dic_rounds
+temp.append(game[5]) #dic_referees
+temp.append(game[2]) #dic_venues
+temp.append(game[0]) #dic_time
+temp.append(game[4]) #dic_formations
 game=temp
 
-gameind = [29, 9, 1, 3, 15, 5, 2, 14]
+dizionari = ds.generate_dictionary(dataset)
+
+game[0] = cerca_stringa_in_dizionario(dizionari[10], game[0])
+game[1] = cerca_stringa_in_dizionario(dizionari[7], game[1])
+game[2] = cerca_stringa_in_dizionario(dizionari[3], game[2])
+game[3] = cerca_stringa_in_dizionario(dizionari[0], game[3])
+game[4] = cerca_stringa_in_dizionario(dizionari[5], game[4])
+game[5] = cerca_stringa_in_dizionario(dizionari[2], game[5])
+game[6] = cerca_stringa_in_dizionario(dizionari[9], game[6])
+
+print(game)
+
+gameind = [29, 9, 3, 15, 5, 2, 14]
 
 gamedict = {colonna: valore for colonna, valore in zip(gameind, game)}
 
